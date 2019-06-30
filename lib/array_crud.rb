48dd65a0d5e811b6c09d5a2f32afef1390003d0d
Steven Hunal <stevehunal@gmail.com>
2019-06-30 14:57:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = my_array []
+  element = "steve"
+  my_array <<
 end
 
 def add_element_to_start_of_array(array, element)
