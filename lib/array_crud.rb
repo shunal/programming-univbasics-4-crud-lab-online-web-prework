305aef1532f9aa4268fd5d7b8c77618b5ea17013
Steven Hunal <stevehunal@gmail.com>
2019-06-30 15:13:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,8 +13,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-   array = ["wow", "I", "am", "really", "learning"]
-    element = "arrays!"
+   array = ["I", "am", "really", "learning"]
+    element = "wow"
     array.unshift(element)
 end
 
